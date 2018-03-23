253db50e045875d316343e39adb8e123da63e1bf
javaxd123 <marcinwarzybok@outlook.com>
2018-03-23 10:43:18 +0100

    durning Security Config

--- src/main/java/com/example/polls/config/SecurityConfig.java
+++ src/main/java/com/example/polls/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.example.polls.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,17 +25,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 )
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //Służy do sprawdzenia roli użytkowników, dostarcza dane użytkowników
     @Autowired
     CustomUserDetailsService customUserDetailsService;
 
+
+    //wyrzucanie błędów do klienta, jeśli ten próbuje dostać się do danych strzeżonych
     @Autowired
     private JwtAuthenticationEntryPoint unautorizedHandler;
 
+
+
+    /*
+        Odczytanie tokenu JWT z nagłówka żądania,
+        Walidacja tokenu,
+        Załadowanie danych użytkownika powiązanych z tokenem,
+        Zapisanie danych użytkownika w kontekście Springa (SecurityContext),
+        Spring używa tych danych do wykonania autoryzacji
+     */
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
         return new JwtAuthenticationFilter();
     }
 
+    //Ustawienie Serwisu użytkowników i dodanie encodera hasła
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
@@ -52,6 +67,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+            http.
+                cors()
+                .and().csrf().disable().exceptionHandling()
+                .authenticationEntryPoint(unautorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .antMatchers("/auth/**")
+                    .permitAll()
+                .antMatchers("/user/checkUsernameAvailability","/user/checkEmailAvailability")
+                    .permitAll()
+                .antMatchers(HttpMethod.GET,"/polls/**","/user/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated();
     }
 }
